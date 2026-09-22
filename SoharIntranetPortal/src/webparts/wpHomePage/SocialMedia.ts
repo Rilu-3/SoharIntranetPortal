0dfeeb7415633ccc981886192e34463c8e7fcad2
Azra Aysha PS <azra.aysha@neologix.in>
2026-09-22 17:29:36 +0530

    updated

---
+++ SoharIntranetPortal/src/webparts/wpHomePage/SocialMedia.ts
@@ -0,0 +1,19 @@
+export default class SocialMedia {
+
+  public static allElementsHtml: string = `
+    <div class="col-12 col-lg-8">
+      <div class="panel-card px-2 py-4">
+
+        <div class="panel-header px-2 w-100 float-start">
+          <h2 class="panel-title">Social Media</h2>
+        </div>
+
+        <div
+          id="social-media-container"
+          class="w-100 float-start px-2 pt-4 panel-card-calendar"
+        ></div>
+
+      </div>
+    </div>
+  `;
+}
